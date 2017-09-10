0038fba709776e32dcd3c5ff02ede8229664c390
AkshatSrivastava2 <akshatsrivastava2@gmail.com>
2017-09-10 18:28:44 +0530

    Migrations for linkedin profile setup

---
+++ database/migrations/2017_09_03_072008_create_companies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('companies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('company_name');
+            $table->string('company_address');
+            $table->integer('user_id');
+            $table->date('started_on');
+            $table->date('ended_on');
+            $table->string('title');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('companies');
+    }
+}
