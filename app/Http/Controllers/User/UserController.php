609a0e91dacda74731244d0def761f370d86e5cb
Saurabh0707 <imtemptedguy@gmail.com>
2017-09-13 21:49:23 +0530

    deleted_login_logout_controller

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -65,15 +65,17 @@ class UserController extends ApiController
         return Route::dispatch($makerequest);       
     }
     
+    /**
+     * Login a user.
+     *
+     */
     public function login(Request $request)
     {
-         
-        $rules = [
+         $rules = [
                         'email'=>'required',
                         'password'=>'required|min:6',
                 ];
-        $this->validate($request,$rules);
-        
+        $this->validate($request,$rules);        
         $params=[
                 'grant_type'=>'password',
                 'client_id'=>$this->client->id,
@@ -82,12 +84,14 @@ class UserController extends ApiController
                 'password'=>request('password'),
                 'scope'=>'*'
             ];
-
-            $request->request->add($params);
-            $makerequest=Request::create('oauth/token','POST');
-            return Route::dispatch($makerequest);        
+        $request->request->add($params);
+        $makerequest=Request::create('oauth/token','POST');
+        return Route::dispatch($makerequest);        
     }
-
+    /**
+     * Refresh A token.
+     *
+     */
     public function refresh(Request $request)
     {
         $this->validate($request,[
@@ -106,10 +110,13 @@ class UserController extends ApiController
         $proxy=Request::create('oauth/token','POST');
         return Route::dispatch($proxy);
     }
-
+    /**
+     * Logot a user.
+     *
+     */
     public function logout(Request $request)
     {
-        Auth::user('api')->token()->revoke();//jo token hai ye usey revoke kro bs
+        Auth::user('api')->token()->revoke();
         Cache::flush();
         return response()->json(['message' => 'User was logged out', 'code'=> 200],200);
     }
